70810274001735c1e490d4b117f53aeb178c6168
Michele Azzolari <michele@azzolari.it>
2017-08-16 16:05:39 +0200

    Adds a script to get a new access_token

---
+++ refreshToken.sh
@@ -0,0 +1,7 @@
+
+curl -H "Authorization: Basic MTcwYjQyMWNhOThlNDA0OWJjOTc2NWNkMTEwZjIyNGI6MTE2ZDg5ZjE4OTdiNGQ4ZjliMDE1NDI2Njk4NWUzZTg=" \
+  -d grant_type=refresh_token \
+  -d refresh_token=AQBPxkb5upY958REoVnUPZxZWy6_iqBCJeCU1UFIR_4tlFELcCSi5L0C8CWehvofuci88u6kfPpqSZemH80tpnzFJamMwMrerNFF098EVDsrjAZ4MO-k6iA6fTW9rhD4t6E \
+  https://accounts.spotify.com/api/token
+
+
